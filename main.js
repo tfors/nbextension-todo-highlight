0a860d0f62af3034556a97ece4b1a51002610dc8
Thomas Fors <tom@fors.net>
2016-03-01 20:24:27 -0600

    Attempt to turn MinRK's scratchpad extension into a todo comment highlight extension.

--- main.js
+++ main.js
@@ -5,106 +5,124 @@ define(['require'], function (require) {
   var events = require('base/js/events');
   var utils = require('base/js/utils');
   var CodeCell = require('notebook/js/codecell').CodeCell;
+  var CodeMirror = require('codemirror/lib/codemirror');
+
+  var todoHighlightOverlay = {
+    token: function(stream) {
+      if (stream.pos == 0) {
+        var re = /\b(to\s*do|fix\s*me|hack)\b/i;
+        var match = re.exec(stream.string);
+        if (match) {
+          stream.skipToEnd();
+          return "highlight";
+        }
+      }
+      stream.skipToEnd();
+    }
+  };
 
 
-  var Scratchpad = function (nb) {
-    var scratchpad = this;
+  var Todo = function (nb) {
+    var todo = this;
     this.notebook = nb;
-    this.kernel = nb.kernel;
-    this.km = nb.keyboard_manager;
-    this.collapsed = true;
+    // this.kernel = nb.kernel;
+    // this.km = nb.keyboard_manager;
+    // this.collapsed = true;
+
+    console.log(CodeCell.code_mirror);
+    CodeCell.prototype.create_element.code_mirror.addOverlay(todoHighlightOverlay);
 
     // create elements
-    this.element = $("<div id='nbextension-scratchpad'>");
-    this.close_button = $("<i>").addClass("fa fa-caret-square-o-down scratchpad-btn scratchpad-close");
-    this.open_button = $("<i>").addClass("fa fa-caret-square-o-up scratchpad-btn scratchpad-open");
-    this.element.append(this.close_button);
-    this.element.append(this.open_button);
-    this.open_button.click(function () {
-      scratchpad.expand();
-    });
-    this.close_button.click(function () {
-      scratchpad.collapse();
-    });
+    // this.element = $("<div id='nbextension-todo-highlight'>");
+    // this.close_button = $("<i>").addClass("fa fa-caret-square-o-down scratchpad-btn scratchpad-close");
+    // this.open_button = $("<i>").addClass("fa fa-caret-square-o-up scratchpad-btn scratchpad-open");
+    // this.element.append(this.close_button);
+    // this.element.append(this.open_button);
+    // this.open_button.click(function () {
+    //   scratchpad.expand();
+    // });
+    // this.close_button.click(function () {
+    //   scratchpad.collapse();
+    // });
 
     // create my cell
-    var cell = this.cell = new CodeCell(nb.kernel, {
-      events: nb.events,
-      config: nb.config,
-      keyboard_manager: nb.keyboard_manager,
-      notebook: nb,
-      tooltip: nb.tooltip,
-    });
-    cell.set_input_prompt();
-    this.element.append($("<div/>").addClass('cell-wrapper').append(this.cell.element));
-    cell.render();
-    cell.refresh();
-    this.collapse();
+    // var cell = this.cell = new CodeCell(nb.kernel, {
+    //   events: nb.events,
+    //   config: nb.config,
+    //   keyboard_manager: nb.keyboard_manager,
+    //   notebook: nb,
+    //   tooltip: nb.tooltip,
+    // });
+    // cell.set_input_prompt();
+    // this.element.append($("<div/>").addClass('cell-wrapper').append(this.cell.element));
+    // cell.render();
+    // cell.refresh();
+    // this.collapse();
 
     // override shift-enter to execute me if I'm focused instead of the notebook's cell
-    var execute_action = this.km.actions.register({
-      handler: $.proxy(this.execute_event, this),
-    }, 'scratchpad-execute');
-    var toggle_action = this.km.actions.register({
-      handler: $.proxy(this.toggle, this),
-    }, 'scratchpad-toggle');
+    // var execute_action = this.km.actions.register({
+    //   handler: $.proxy(this.execute_event, this),
+    // }, 'scratchpad-execute');
+    // var toggle_action = this.km.actions.register({
+    //   handler: $.proxy(this.toggle, this),
+    // }, 'scratchpad-toggle');
     
-    var shortcuts = {
-      'shift-enter': execute_action,
-      'ctrl-b': toggle_action,
-    }
-    this.km.edit_shortcuts.add_shortcuts(shortcuts);
-    this.km.command_shortcuts.add_shortcuts(shortcuts);
+    // var shortcuts = {
+    //   'shift-enter': execute_action,
+    //   'ctrl-b': toggle_action,
+    // }
+    // this.km.edit_shortcuts.add_shortcuts(shortcuts);
+    // this.km.command_shortcuts.add_shortcuts(shortcuts);
 
     // finally, add me to the page
-    $("body").append(this.element);
+    // $("body").append(this.element);
   };
 
-  Scratchpad.prototype.toggle = function () {
-    if (this.collapsed) {
-      this.expand();
-    } else {
-      this.collapse();
-    }
-    return false;
-  };
+  // Scratchpad.prototype.toggle = function () {
+  //   if (this.collapsed) {
+  //     this.expand();
+  //   } else {
+  //     this.collapse();
+  //   }
+  //   return false;
+  // };
 
-  Scratchpad.prototype.expand = function () {
-    this.collapsed = false;
-    var site_height = $("#site").height();
-    this.element.animate({
-      height: site_height,
-    }, 200);
-    this.open_button.hide();
-    this.close_button.show();
-    this.cell.element.show();
-    this.cell.focus_editor();
-    $("#notebook-container").css('margin-left', 0);
-  };
+  // Scratchpad.prototype.expand = function () {
+  //   this.collapsed = false;
+  //   var site_height = $("#site").height();
+  //   this.element.animate({
+  //     height: site_height,
+  //   }, 200);
+  //   this.open_button.hide();
+  //   this.close_button.show();
+  //   this.cell.element.show();
+  //   this.cell.focus_editor();
+  //   $("#notebook-container").css('margin-left', 0);
+  // };
 
-  Scratchpad.prototype.collapse = function () {
-    this.collapsed = true;
-    $("#notebook-container").css('margin-left', 'auto');
-    this.element.animate({
-      height: 0,
-    }, 100);
-    this.close_button.hide();
-    this.open_button.show();
-    this.cell.element.hide();
-  };
+  // Scratchpad.prototype.collapse = function () {
+  //   this.collapsed = true;
+  //   $("#notebook-container").css('margin-left', 'auto');
+  //   this.element.animate({
+  //     height: 0,
+  //   }, 100);
+  //   this.close_button.hide();
+  //   this.open_button.show();
+  //   this.cell.element.hide();
+  // };
 
-  Scratchpad.prototype.execute_event = function (evt) {
-    if (utils.is_focused(this.element)) {
-      this.cell.execute();
-    } else {
-      this.notebook.execute_cell_and_select_below();
-    }
-  };
+  // Scratchpad.prototype.execute_event = function (evt) {
+  //   if (utils.is_focused(this.element)) {
+  //     this.cell.execute();
+  //   } else {
+  //     this.notebook.execute_cell_and_select_below();
+  //   }
+  // };
 
-  function setup_scratchpad () {
+  function setup_todo () {
     // lazy, hook it up to Jupyter.notebook as the handle on all the singletons
-    console.log("Setting up scratchpad");
-    return new Scratchpad(Jupyter.notebook);
+    console.log("Setting up todo extension");
+    return new Todo(Jupyter.notebook);
   }
 
   function load_extension () {
@@ -112,13 +130,13 @@ define(['require'], function (require) {
     var link = document.createElement("link");
     link.type = "text/css";
     link.rel = "stylesheet";
-    link.href = require.toUrl("./scratchpad.css");
+    link.href = require.toUrl("./todo.css");
     document.getElementsByTagName("head")[0].appendChild(link);
     // load when the kernel's ready
     if (Jupyter.notebook.kernel) {
-      setup_scratchpad();
+      setup_todo();
     } else {
-      events.on('kernel_ready.Kernel', setup_scratchpad);
+      events.on('kernel_ready.Kernel', setup_todo);
     }
   }
 
